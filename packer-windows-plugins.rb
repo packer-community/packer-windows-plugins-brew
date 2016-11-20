af35a7805f8839a2d41f903fe05ae33ae45ee913
Edward M. Meshuris <edward@theemm.com>
2016-11-20 11:28:23 -0800

    updated to sha256 and another deprecated statement

--- packer-windows-plugins.rb
+++ packer-windows-plugins.rb
@@ -4,12 +4,12 @@ class PackerWindowsPlugins < Formula
   homepage "https://github.com/packer-community/packer-windows-plugins"
   version "1.0.0"
 
-  if Hardware.is_64_bit?
+  if Hardware::CPU.is_64_bit?
     url "https://github.com/packer-community/packer-windows-plugins/releases/download/v1.0.0/darwin_amd64.zip"
-    sha1 "e09e2ab6f7fc39237b5b41f53aa5b2a815428cfc"
+    sha256 "5fec94f41cb04c5fa5e38bd115f98b0ae1902083d9f281dd880cb6449756f312"
   else
     url "https://github.com/packer-community/packer-windows-plugins/releases/download/v1.0.0/darwin_386.zip"
-    sha1 '64535683e7f261091629c5a96236263dc0856c63'
+    sha256 'c5a3bd15020d3986769d46f8f2c9e9c0ab2c93b7a0b15d08097f6f69f0187781'
   end
 
   depends_on :arch => :intel
